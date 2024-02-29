e73057e798eca5fe097de9973c607d93930857ea
lagoracodes <lagoracodes@gmail.com>
2024-02-29 15:20:44 +0100

    update header and main, 29-02-24

--- GameHub-cleaned-up/js/script.js
+++ GameHub-cleaned-up/js/script.js
@@ -1,1 +1,27 @@
-console.log("hello world!");
+const body = document.querySelector("body");
+const btnHamburger = document.querySelector("#btnHamburger");
+const header = document.querySelector("header");
+const overlay = document.querySelector(".overlay");
+const hasFade = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("click hamburger");
+
+  if (header.classList.contains("open")) {
+    // close the mobile menu
+    body.classList.remove("no-scroll");
+    header.classList.remove("open");
+    hasFade.forEach(function (element) {
+      element.classList.add("fade-out");
+      element.classList.remove("fade-in");
+    });
+  } else {
+    // open the mobile menu
+    body.classList.add("no-scroll");
+    header.classList.add("open");
+    hasFade.forEach(function (element) {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
+  }
+});
