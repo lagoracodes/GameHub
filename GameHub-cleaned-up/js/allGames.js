55f3a435734cf376b6303049bb1aec733a539619
lagoracodes <lagoracodes@gmail.com>
2024-04-02 23:52:14 +0200

    update lower panel, add game info button

--- GameHub-cleaned-up/js/allGames.js
+++ GameHub-cleaned-up/js/allGames.js
@@ -9,12 +9,14 @@ for (let i = 0; i < usableData.length; i++) {
   const gameTitle = usableData[i].title;
   const price = usableData[i].discountedPrice;
 
+  console.log(gameTitle.length);
+
   loadingCircle.remove();
   allGames.classList.remove("flex");
   allGames.classList.add("grid-wrapper");
 
   allGames.innerHTML += `
-    <div class="game-card flex">
+    <div class="game-card flex f-ai-c">
       <div class="img-wrapper">
         <img
           class="game-picture"
@@ -22,9 +24,14 @@ for (let i = 0; i < usableData.length; i++) {
           alt="video game cover picture"
         />
       </div>
-        <div class="lower-panel">
-          <h3 class="game-title letter-spacing">${gameTitle}</h3>
-          <div class="flex f-jc-c f-ai-c">
+        <div class="flex lower-panel">
+          <h3 id="game-title" class="game-title letter-spacing">${gameTitle}</h3>
+      
+              <a class="btn-2 flex f-ai-c f-jc-c" href="#">
+                <span class="btn-span uppercase letter-spacing">game info</span>
+              </a>
+
+          <div class="flex f-jc-c f-ai-c gap">
             <div>
               <h4 class="uppercase letter-spacing">
                 current <br />
@@ -32,16 +39,23 @@ for (let i = 0; i < usableData.length; i++) {
               </h4>
               <h3 class="letter-spacing">${price}</h3>
             </div>
-            <button>
-              <a class="uppercase letter-spacing" href="#">
-                browse <br />
-                sellers
+            <a class="btn-1 flex f-ai-c f-jc-c" href="#">
+                <span class="btn-span uppercase letter-spacing">browse <br />
+                sellers</span>
               </a>
-            </button>
           </div>
       </div>
     </div>
       `;
+
+  const gameTitleH3 = document.querySelectorAll("#game-title");
+  if (gameTitleH3[i].innerText.length >= 20) {
+    gameTitleH3[i].classList.add("margin");
+
+    console.log("hi, ", gameTitle, " ", gameTitleH3);
+  } else {
+    continue;
+  }
 }
 
 // allGames.innerHTML +=
